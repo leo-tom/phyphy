93acf8b60a6b9256ee40092ace4114d7054fc810
leo-tom <coder.le0.1060@gmail.com>
2017-02-06 23:09:08 +0900

    Add files via upload

---
+++ AwkInput.java
@@ -0,0 +1,76 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+
+public class AwkInput extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTextArea textArea;
+	private ArrayList<ActionListener> listeners = new ArrayList<>();
+
+	/**
+	 * Create the dialog.
+	 */
+	public AwkInput() {
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		{
+			JScrollPane scrollPane = new JScrollPane();
+			contentPanel.add(scrollPane, BorderLayout.CENTER);
+			{
+				textArea = new JTextArea();
+				textArea.setTabSize(2);
+				scrollPane.setViewportView(textArea);
+			}
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("Execute");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						for (ActionListener actionListener : listeners) {
+							actionListener.actionPerformed(null);
+							setVisible(false);
+						}
+					}
+				});
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						setVisible(false);
+					}
+				});
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+		this.setVisible(true);
+	}
+	public String getText(){
+		return textArea.getText();
+	}
+	public void addExecutelistener(ActionListener adding){
+		listeners.add(adding);
+	}
+
+}
